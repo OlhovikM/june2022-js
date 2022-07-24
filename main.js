4a145925484237cb6219abc572678f3c13b34f62
Maria <Olhovikmaria14@gmail.com>
2022-07-24 21:13:01 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,97 @@
+//Task 1
+let word_1 = `hello`;
+console.log(word_1);
+alert(word_1);
+document.write(word_1 + " ");
+
+let word_2 = `owu`;
+console.log(word_2);
+alert(word_2);
+document.write(word_2 + " ");
+
+let word_3 = `com`;
+console.log(word_3);
+alert(word_3);
+document.write(word_3 + " ");
+
+let word_4 = `ua`;
+console.log(word_4);
+alert(word_4);
+document.write(word_4 + " ");
+
+let number_1;
+number_1 = 1;
+console.log(number_1);
+alert(number_1);
+document.write(number_1 + " ");
+
+let number_2;
+number_2 = 10;
+console.log(number_2);
+alert(number_2);
+document.write(number_2 + " ");
+
+let number_3;
+number_3 = -999;
+console.log(number_3);
+alert(number_3);
+document.write(number_3 + " ");
+
+let number_4;
+number_4 = 123;
+console.log(number_4);
+alert(number_4);
+document.write(number_4 + " ");
+
+let number_5;
+number_5 = 3.14;
+console.log(number_5);
+alert(number_5);
+document.write(number_5 + " ");
+
+let number_6;
+number_6 = 2.7;
+console.log(number_6);
+alert(number_6);
+document.write(number_6 + " ");
+
+let number_7;
+number_7 = 16;
+console.log(number_7);
+alert(number_7);
+document.write(number_7 + " ");
+
+let bool_1;
+bool_1 = true;
+console.log(bool_1);
+alert(bool_1);
+document.write(bool_1 + " ");
+
+let bool_2;
+bool_2 = false;
+console.log(bool_2);
+alert(bool_2);
+document.write(bool_2 + " ");
+
+//Task 2
+let firstName= `Olkhovik `;
+let middleName = `Maria `;
+let lastName = `Vasylivna`;
+let person = firstName + middleName + lastName;
+console.log(person);
+
+//Task 3
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//Task 4
+let person_name = prompt("Enter your name:");
+console.log(person_name);
+let person_lastName = prompt("Enter your lastname:");
+console.log(person_lastName);
+let person_age = prompt("Enter your age:");
+console.log(person_age);
