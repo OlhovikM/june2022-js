6d15d53fc364138bc19275b712d026da77b12b53
Maria <Olhovikmaria14@gmail.com>
2022-07-27 22:38:23 +0300

    Add lesson 2

---
+++ lesson2/main.js
@@ -0,0 +1,53 @@
+//Task 1
+let country = ['USA', 'Canada', 'Italy', 'Ukraine', 'Germany', 'Switzerland', 'Mexico', 'France', 'Spain', 'Norvey' ];
+console.log(country);
+console.log(country[0]);
+console.log(country[1]);
+console.log(country[2]);
+console.log(country[3]);
+console.log(country[4]);
+console.log(country[5]);
+console.log(country[6]);
+console.log(country[7]);
+console.log(country[8]);
+console.log(country[9]);
+//Task 2
+let books = {
+    title: 'Misery',
+    pageCount: 350,
+    genre: 'thriller'
+    }
+    console.log(books);
+//Task 3
+let books_1 = {
+    title: 'Conspiracy',
+    pageCount: 270,
+    genre: 'detective',
+    authors: [
+        {name: 'Steven King', age: 74},
+        {name: 'Agata Kristy', age: 86},
+        {name: 'Dan Braun', age: 58},
+    ]
+}
+console.log(books_1);
+//Task 4
+let users = [
+    {
+        name: 'Maria',
+        username: 'Mar_ia',
+        password: 869500
+    },
+    {
+        name: 'Vlad',
+        username: 'Killer',
+        password: 134555
+    },
+    {
+        name: 'Sofia',
+        username: 'Phantom',
+        password: 111122
+    }
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
